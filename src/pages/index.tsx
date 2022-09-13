12548ef3b5ba72f3518c2373d316da70983e6ee6
k1nho <kinhong99@gmail.com>
2022-09-13 16:23:32 -0400

    Using the trpc query in the FE

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,64 @@
 import { signIn , signOut, useSession} from "next-auth/react";
 import { NextPage } from "next";
+import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
   const {data: session, status} = useSession();
 
   if(status === "loading"){
     return(
-      <main>
+      <main className="flex flex-col items-center pt-4">
         Loading ...
       </main>
     )
   }
 
   return(
-    <main>
-      <h1>Guesbook</h1>
+    <main className="flex flex-col items-center">
+      <h1 className="text-3xl pt-4">Guesbook</h1>
       {
         session ? (
-          <div>
+          <div className="pt-10">
             <p>Hello {session.user?.name}</p>
             <button onClick={() => signOut()}>Logout</button>
+            <div className="pt-10">
+              <Messages/>
+            </div>
           </div>
         ) : (
           <div>
             <button onClick={() => signIn("discord")}>
               Login With Discord
             </button>
+            <div className="pt-10">
+              <Messages/>
+            </div>
           </div>
         )
       }
     </main>
   );
 };
+
+const Messages : React.FC = () =>{
+  const {data: messages, isLoading} = trpc.useQuery(["guestbook.getAll"])
+
+  if(isLoading) return <div>Fetching Messages</div>
+
+  return(
+    <div className="flex flex-col gap-4">
+      {messages?.map((msg, index) => {
+        return(
+          <div key={index}>
+            <p>{msg.message}</p>
+            <p>- {msg.name}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+
 
 export default Home;
