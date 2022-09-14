717a91999401fc29fe4dcddc94698756a6f6cfc7
k1nho <kinhong99@gmail.com>
2022-09-14 16:43:51 -0400

    Merge into app router, todo router

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,11 +2,13 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 import { guestBookRouter } from "./guestbook";
+import { todoRouter } from "./todo";
 
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge("guestbook.", guestBookRouter)
+  .merge("todo.", todoRouter)
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
